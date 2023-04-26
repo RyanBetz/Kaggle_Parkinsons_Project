514c3bb8d4d74ab356258832a3e9aed34045a707
ryanbetz <ryanbetz8484@gmail.com>
2023-04-26 11:25:33 -0400

    upload

--- parkinsons_model.py
+++ parkinsons_model.py
@@ -17,10 +17,13 @@ train_peptides_grouped = train_peptides.groupby(['patient_id', 'visit_month']).a
 print(train_peptides_grouped.head())
 
 # Filter data for a single patient
-patient_id = 'PATIENT_ID_HERE'
-patient_df = df[df['patient_id'] == patient_id]
+patient_id = '35'
+patient_df = train_peptides[train_peptides['patient_id'] == patient_id]
 
 # Create scatter plot
 sns.scatterplot(x='visit_month', y='PeptideAbundance', data=patient_df)
 plt.title(f'Peptide Abundance vs. Visit Month for Patient {patient_id}')
+plt.xlabel('Visit Month')
+plt.ylabel('Peptide Abundance')
+
 plt.show()

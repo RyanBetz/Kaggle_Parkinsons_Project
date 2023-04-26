5ad880ce169182a7ae2a484ac6b7bd7c1322d841
ryanbetz <ryanbetz8484@gmail.com>
2023-04-26 14:13:17 -0400

    upload

--- parkinsons_model.py
+++ parkinsons_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from functools import reduce
 import numpy as np
 # Read in the CSV file
 train_peptides = pd.read_csv('amp-parkinsons-disease-progression-prediction/train_peptides.csv')
@@ -76,11 +77,16 @@ plt.ylabel('Correlation with UPDRS Part 3')
 plt.title('Top 10 Unique Proteins Correlated with UPDRS Part 3')
 plt.show()
 
-# Merge the datasets using an outer join on visit_id
-merged_df = pd.merge(train_proteins, train_clinical_data, on=['visit_id', 'visit_month', 'patient_id'])
-merged_df = pd.merge(merged_df, train_peptides, on=['visit_id', 'visit_month', 'patient_id'])
-merged_df = pd.merge(merged_df, train_supplemental_clinical_data, on='visit_id', how='outer', suffixes=('_scd', '_scd'))
+# Read in the CSV files
+df_list = []
+for file_name in ['amp-parkinsons-disease-progression-prediction/train_peptides.csv', 'amp-parkinsons-disease-progression-prediction/train_proteins.csv', 'amp-parkinsons-disease-progression-prediction/train_clinical_data.csv', 'amp-parkinsons-disease-progression-prediction/supplemental_clinical_data.csv']:
+    df = pd.read_csv(file_name)
+    df_list.append(df)
+
+# Merge the dataframes using reduce and specify suffixes for duplicate columns
+merged_df = reduce(lambda left, right: pd.merge(left, right, on='visit_id', how='outer', suffixes=('_clin', '_supp')), df_list)
 
 # Print the resulting dataframe
 print(merged_df.head())
+
 
